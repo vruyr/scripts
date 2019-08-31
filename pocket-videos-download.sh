bbb0fec65e46043df69142982b2b024608152b8a
Vruyr Gyolchanyan <vruyr@vruyr.com>
2019-08-31 16:01:02 -0400

    Added a script wrapping getpocket and youtube-dl that downloads videos.

---
+++ pocket-videos-download.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+item_ids=()
+
+
+IFS=$'\t'
+while read item_id given_url resolved_title; do
+	resolved_title=${resolved_title//:/_}
+	resolved_title=${resolved_title//\//_}
+
+	# youtube-dl --format bestvideo+bestaudio --output "%(upload_date)s %(extractor)s-%(id)s [%(uploader)s] %(title)s.%(ext)s"
+	youtube-dl --format bestvideo+bestaudio --output "%(upload_date)s %(extractor)s-%(id)s [%(uploader)s] ${resolved_title}.%(ext)s" "${given_url}"
+
+	item_ids+=( "$item_id" )
+done < <(getpocket list "$@" --format $'{item_id}\t{given_url}\t{resolved_title}\n')
+
+
+printf "\n\n\n"
+printf "getpocket delete"
+printf " %q" "${item_ids[@]}"
+printf "\n\n\n"
