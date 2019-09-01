72345fa79d7a78de33098af82546055fa7812ced
Vruyr Gyolchanyan <vruyr@vruyr.com>
2019-09-01 08:25:17 -0400

    Updated downloaded file name format.

--- pocket-videos-download.sh
+++ pocket-videos-download.sh
@@ -9,7 +9,7 @@ while read item_id given_url resolved_title; do
 	resolved_title=${resolved_title//\//_}
 
 	# youtube-dl --format bestvideo+bestaudio --output "%(upload_date)s %(extractor)s-%(id)s [%(uploader)s] %(title)s.%(ext)s"
-	youtube-dl --format bestvideo+bestaudio --output "%(upload_date)s %(extractor)s-%(id)s [%(uploader)s] ${resolved_title}.%(ext)s" "${given_url}"
+	youtube-dl --format bestvideo+bestaudio --output "%(upload_date)s %(duration) 5ds [%(uploader)s] ${resolved_title}.%(extractor)s.%(id)s.%(ext)s" "${given_url}"
 
 	item_ids+=( "$item_id" )
 done < <(getpocket list "$@" --format $'{item_id}\t{given_url}\t{resolved_title}\n')
