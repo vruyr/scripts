d941cfd52932239ed62a132be9c3072366acc19f
Vruyr Gyolchanyan <vruyr@vruyr.com>
2019-09-29 12:21:46 -0400

    Updated video filename encoding of special chars.

--- pocket-videos-download.sh
+++ pocket-videos-download.sh
@@ -15,6 +15,7 @@ while read item_id given_url resolved_title; do
 	resolved_title=${resolved_title//\?/%3F}
 	resolved_title=${resolved_title//\//%2F}
 	resolved_title=${resolved_title//\|/%7C}
+	resolved_title=${resolved_title//\"/%22}
 
 	youtube-dl --format 'bestvideo[ext=mp4]+bestaudio[ext=m4a]' --output "%(uploader)s/%(upload_date)s %(duration) 5ds ${resolved_title}.%(extractor)s.%(id)s.%(ext)s" "${given_url}"
 
