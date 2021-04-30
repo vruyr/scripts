60cb8941de9f5085e7045035a523056ca1f8f901
Vruyr Gyolchanyan <vruyr@vruyr.com>
2021-04-30 19:49:11 -0400

    compare-trashed-receipts.py

--- compare-trashed-receipts.py
+++ compare-trashed-receipts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Python Standard Library
-import sys; assert sys.version_info[:2] in [(3, 7), (3, 8)]
+import sys; assert sys.version_info[:2] in [(3, 7), (3, 8), (3, 9)]
 import asyncio, subprocess, os, pathlib, shlex
 
 
