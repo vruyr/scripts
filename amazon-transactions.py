72cedaf1e503d2ecdfdf474363bf0fe76329a916
Vruyr Gyolchanyan <vruyr@vruyr.com>
2021-09-12 17:20:06 -0400

    amazon-transactions.py
    Improved open_amazon_order_page.

--- amazon-transactions.py
+++ amazon-transactions.py
@@ -4,14 +4,85 @@ import sys, re, time, subprocess
 
 
 open_amazon_order_page = """
-on run argv
-	set orderId to item 1 of argv
+-- https://alexwlchan.net/2020/06/using-applescript-to-open-a-url-in-private-browsing-in-safari/
+on openUrlInNonPrivateWindow(urlToOpen)
 	tell application "Safari"
 		activate
-		set URL of document 1 to ("https://www.amazon.com/gp/css/summary/print.html?orderID=" & orderId)
-		set doc to document ("Amazon.com - Order " & orderId)
-		tell doc to activate
+
+		tell application "System Events"
+			click menu item "New Window" of ¬
+				menu "File" of menu bar 1 of ¬
+				application process "Safari"
+		end tell
+
+		-- The frontmost window is the Non-Private Browsing window that just got opened -- change the URL to the one we want to open.
+		tell window 1 to set properties of current tab to {URL:urlToOpen}
 	end tell
+end openUrlInNonPrivateWindow
+
+on openUrlInNewTab(theUrl)
+	tell application "Safari"
+		activate
+		if (count of windows) < 1 then
+			my openUrlInNonPrivateWindow(theUrl)
+		else
+			tell window 1
+				set current tab to (make new tab with properties {URL:theUrl})
+			end tell
+		end if
+	end tell
+end openUrlInNewTab
+
+-- https://alexwlchan.net/2021/04/detect-private-browsing/
+on isFrontmostSafariWindowPrivateBrowsing()
+	-- If you don't activate Safari, the results are sometimes wrong.
+	-- In particular, System Events doesn't have the most up-to-date
+	-- information about the state of the menu.
+	--
+	-- I think this is the same problem as described in
+	-- https://www.reddit.com/r/applescript/comments/an1cpj/information_in_system_events_not_updating/
+	tell application "Safari" to activate
+
+	tell application "System Events"
+		set theWindowMenu to menu "Window" of ¬
+			menu bar 1 of ¬
+			application process "Safari"
+
+		return (menu item "Move Tab to New Private Window" of theWindowMenu) exists
+	end tell
+end isFrontmostSafariWindowPrivateBrowsing
+
+on getFrontmostSafariDocumentUrl()
+	tell application "Safari"
+		activate
+		if (count of documents) < 1 then
+			return ""
+		end if
+		return URL of document 1
+	end tell
+end getFrontmostSafariDocumentUrl
+
+on setFrontmostSafariDocumentUrl(theUrl)
+	tell application "Safari"
+		activate
+		set URL of document 1 to theUrl
+	end tell
+end setFrontmostSafariDocumentUrl
+
+on run argv
+	set orderId to item 1 of argv
+	set theUrlBase to "https://www.amazon.com/gp/css/summary/print.html?orderID="
+	set theUrl to (theUrlBase & orderId)
+
+	if isFrontmostSafariWindowPrivateBrowsing() then
+		openUrlInNonPrivateWindow(theUrl)
+	else
+		if getFrontmostSafariDocumentUrl() starts with theUrlBase then
+			setFrontmostSafariDocumentUrl(theUrl)
+		else
+			openUrlInNewTab(theUrl)
+		end if
+	end if
 end run
 """
 
