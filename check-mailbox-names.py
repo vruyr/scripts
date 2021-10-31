d9ba23b4c22eda98bfd0fae503a7ffb35910ffbd
Vruyr Gyolchanyan <vruyr@vruyr.com>
2021-10-30 20:14:00 -0400

    check-mailbox-names.py
    Moved accounts into a configuration file.

--- check-mailbox-names.py
+++ check-mailbox-names.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
-import pathlib, itertools, collections, json, sys, subprocess
+from os import path
+import pathlib, collections, json, subprocess
+
+
+CONFIG_DIR = pathlib.Path.home() / ".config" / pathlib.Path(__file__).stem
 
 
 def main():
 	folders = collections.defaultdict(list)
 
-	for server, username in [
-		["10.0.7.10", "vruyr@mechena.com"],
-		["imap.gmail.com", "vruyr@vruyr.com"]
-	]:
+	for label, server, username in read_config("accounts"):
 		p = subprocess.run(
 			["imap.py", "-l", "--json", "-s", server, "-u", username],
 			stdout=subprocess.PIPE,
@@ -20,17 +21,20 @@ def main():
 		assert not p.stderr
 		for entry in json.loads(p.stdout.decode()):
 			path = entry["path"]
-			path.insert(0, username)
+			path.insert(0, label)
 			for p in path:
 				assert "/" not in p
 			folders[path[-1]].append("/".join(path))
 
 	folders = {name: paths for name, paths in folders.items() if len(paths) != 1}
-	for paths in folders.values():
-		if len(paths) == 1:
-			continue
-		print("\n".join(paths), end="\n\n")
-	# json.dump(folders, sys.stdout, indent=4)
+	print("\n\n".join(
+		"\n".join(paths) for paths in folders.values()
+	))
+
+
+def read_config(name):
+	with (CONFIG_DIR / (name + ".json")).open("r") as fo:
+		return json.load(fo)
 
 
 if __name__ == '__main__':
