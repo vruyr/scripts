75a7eeda0366d2c0110521ea93c1a81e5227df88
Vruyr Gyolchanyan <vruyr@vruyr.com>
2022-03-06 10:28:17 -0500

    compare-trashed-receipts.py
    Updated Python version check.

--- compare-trashed-receipts.py
+++ compare-trashed-receipts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # Python Standard Library
-import sys; assert sys.version_info[:2] in [(3, 7), (3, 8), (3, 9)]
+import sys; assert sys.version_info[0] == 3 and sys.version_info[1] >= 7
 import asyncio, subprocess, os, pathlib, shlex
 
 
