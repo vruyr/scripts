9114ea1a5670dfe7677d4794f62a9ad79b3133af
Vruyr Gyolchanyan <vruyr@vruyr.com>
2023-02-15 19:42:04 -0500

    check-mailbox-names.py
    - Added support for `--show-all-mailboxes` command line flag.

--- check-mailbox-names.py
+++ check-mailbox-names.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
 
-from os import path
-import pathlib, collections, json, subprocess
+import sys, locale, argparse, pathlib, collections, json, subprocess
 
 
 CONFIG_DIR = pathlib.Path.home() / ".config" / pathlib.Path(__file__).stem
 
 
-def main():
-	folders = collections.defaultdict(list)
+def main(
+	*,
+	# Example Options
+	show_all_mailboxes
+):
+	locale.setlocale(locale.LC_ALL, "")
 
-	ignored_paths = set(tuple(path) for path in read_config("ignore"))
+	if show_all_mailboxes:
+		for label, entries in list_accounts():
+			print(label)
+			for entry in entries:
+				print("\t", "/".join(entry["path"]), sep="")
+			print()
+	else:
+		folders = collections.defaultdict(list)
+		ignored_paths = set(tuple(path) for path in read_config("ignore"))
+		for label, entries in list_accounts():
+			for entry in entries:
+				path = entry["path"]
+				if tuple(path) in ignored_paths:
+					continue
+				path.insert(0, label)
+				for p in path:
+					assert "/" not in p
+				folders[path[-1]].append("/".join(path))
 
+		folders = {name: paths for name, paths in folders.items() if len(paths) != 1}
+		print("\n\n".join(
+			"\n".join(paths) for paths in folders.values()
+		))
+
+
+def list_accounts():
 	for label, url in read_config("accounts"):
 		p = subprocess.run(
 			["imap.py", "-a", url, "--json"],
@@ -21,19 +48,7 @@ def main():
 		)
 		assert p.returncode == 0, p
 		assert not p.stderr
-		for entry in json.loads(p.stdout.decode()):
-			path = entry["path"]
-			if tuple(path) in ignored_paths:
-				continue
-			path.insert(0, label)
-			for p in path:
-				assert "/" not in p
-			folders[path[-1]].append("/".join(path))
-
-	folders = {name: paths for name, paths in folders.items() if len(paths) != 1}
-	print("\n\n".join(
-		"\n".join(paths) for paths in folders.values()
-	))
+		yield (label, json.loads(p.stdout.decode()))
 
 
 def read_config(name):
@@ -41,5 +56,51 @@ def read_config(name):
 		return json.load(fo)
 
 
-if __name__ == '__main__':
-	main()
+def parse_args(*, args, prog):
+	parser = argparse.ArgumentParser(
+		prog=prog,
+		usage="%(prog)s [OPTIONS]...",
+		description=__doc__,
+		formatter_class=argparse.RawTextHelpFormatter,
+		fromfile_prefix_chars="@",
+		add_help=False,
+	)
+
+	the_default = "\ndefault: %(default)s"
+
+	options_generic = parser.add_argument_group("Generic Options")
+	options_generic.add_argument(
+		"--help", "-h",
+		action="help",
+		help="show help message and exit",
+	)
+
+	options_example = parser.add_argument_group("Example Options")
+	options_example.add_argument(
+		"--show-all-mailboxes",
+		action="store_true", dest="show_all_mailboxes", default=False, required=False,
+		help="instead of finding mailboxes with same name, simply list all mailboxes in all accounts" + the_default
+	)
+
+	opts = parser.parse_args(args)
+	return vars(opts)
+
+
+def configure_logging(opts):
+	pass
+
+
+def smain(argv=None):
+	if argv is None:
+		argv = sys.argv
+
+	try:
+		opts = parse_args(args=argv[1:], prog=argv[0])
+		configure_logging(opts)
+		return main(**opts)
+	except KeyboardInterrupt:
+		print(file=sys.stderr)
+
+
+if __name__ == "__main__":
+	sys.exit(smain())
