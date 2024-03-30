812bbb5ba65df41130a31aa9018cd9011200cc52
Vruyr Gyolchanyan <vruyr@vruyr.com>
2024-03-30 11:20:44 -0400

    print-year-and-week.swift
    Changed output format to "YY'W'ww'D'e" which is like "24W13D7".

--- print-year-and-week.swift
+++ print-year-and-week.swift
@@ -3,9 +3,13 @@
 import Foundation
 
 let now = Date()
+
+//TODO Make sure Sunday is the the first day of the week and the week number number is following U.S. conventions.
 let formatter = DateFormatter()
 formatter.locale = Locale(identifier: "en_US")
 // https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/DataFormatting/Articles/dfDateFormatting10_4.html
 // https://www.unicode.org/reports/tr35/tr35-31/tr35-dates.html#Date_Format_Patterns
-formatter.dateFormat = "YY'W'ww"
-print(formatter.string(from: now))
+//formatter.dateFormat = "YY'W'wwEEEEEE"
+formatter.dateFormat = "YY'W'ww'D'e"
+
+print(formatter.string(from: now).uppercased())
