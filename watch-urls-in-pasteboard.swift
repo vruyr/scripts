4cb4947128ff4d7863bc55c9ed1ddb8f8372d1aa
Vruyr Gyolchanyan <vruyr@vruyr.com>
2025-07-19 16:09:45 -0400

    watch-urls-in-pasteboard.swift: Implemented command line argument parsing and un-hardcoded the command to execute.

--- watch-urls-in-pasteboard.swift
+++ watch-urls-in-pasteboard.swift
@@ -4,7 +4,13 @@ import Foundation
 import AppKit
 
 
-func main() async {
+struct CliOptions {
+	var commandToExec: [String]
+	var numLinesToShow: UInt = 5
+}
+
+
+func main(options: CliOptions) async {
 	signal(SIGINT) { signal in
 		print("\nBye...")
 		exit(0)
@@ -13,6 +19,9 @@ func main() async {
 	signal(SIGWINCH) { signal in
 		print("\nTerminal Size Changed")
 	}
+
+	let commandToExecCmd: URL = resolveCommand(options.commandToExec.first!)
+	print("Resolved command path: \(commandToExecCmd.path)")
 
 	let pasteboard = NSPasteboard.general
 	var lastChangeCount = pasteboard.changeCount
@@ -42,16 +51,18 @@ func main() async {
 			continue
 		}
 
+		let commandToExecArgs: [String] = options.commandToExec.dropFirst().map {
+			$0 == "{}" ? pasteboardContent : $0
+		}
+
 		Task {
 			print("\(outputPrefix)\(pasteboardContent)")
 			do {
 				let exitCode = try await runCommand(
-					"/Users/vruyr/.bin/local-scripts/yt-dlp",
-					arguments: [
-						"--simulate",
-						pasteboardContent
-					],
-					prefix: "\(outputPrefix)"
+					commandToExecCmd,
+					arguments: commandToExecArgs,
+					prefix: "\(outputPrefix)",
+					numLinesToShow: options.numLinesToShow
 				)
 				print("\(outputPrefix)Exit Code: \(exitCode)")
 			} catch {
@@ -69,10 +80,45 @@ func isValidURL(_ text: String) -> Bool {
 	return true
 }
 
+func isExecutableFile(atPath path: String) -> Bool {
+	var isDirectory: ObjCBool = false
+	let exists = FileManager.default.fileExists(atPath: path, isDirectory: &isDirectory)
+	return (
+		exists
+		&& !isDirectory.boolValue
+		&& FileManager.default.isExecutableFile(atPath: path)
+	)
+}
 
-func runCommand(_ command: String, arguments: [String], prefix: String) async throws -> Int32 {
+func resolveCommand(_ command: String) -> URL {
+	// If already an absolute or relative path, use as is.
+	if command.contains("/"){
+		let fileURL = URL(fileURLWithPath: command)
+		if !isExecutableFile(atPath: fileURL.path) {
+			print("ERROR: The path \(String(reflecting: command)) is not executable file.")
+			exit(2)
+		}
+		return fileURL
+	}
+
+	// Otherwise, search in PATH
+
+	let envPath = ProcessInfo.processInfo.environment["PATH"] ?? ""
+	for dir in envPath.split(separator: ":") {
+		let candidate = URL(fileURLWithPath: String(dir)).appendingPathComponent(command)
+		if FileManager.default.isExecutableFile(atPath: candidate.path) {
+			return candidate
+		}
+	}
+
+	print("ERROR: An executable \(String(reflecting: command)) was not found in PATH.")
+	exit(3)
+}
+
+
+func runCommand(_ command: URL, arguments: [String], prefix: String, numLinesToShow: UInt) async throws -> Int32 {
 	let process = Process()
-	process.executableURL = URL(fileURLWithPath: command)
+	process.executableURL = command
 	process.arguments = arguments
 
 	let pipe = Pipe()
@@ -83,14 +129,13 @@ func runCommand(_ command: String, arguments: [String], prefix: String) async th
 	try process.run()
 
 	do {
-		let numLinesToKeep: Int32 = 5
 		var outputLines: [String] = []
 		for try await addedLine in fileHandle.bytes.lines {
 			outputLines.append(addedLine)
 			var printFrom = outputLines.count - 1
-			if outputLines.count > numLinesToKeep {
+			if outputLines.count > numLinesToShow {
 				outputLines.removeFirst()
-				print("\u{001B}[\(numLinesToKeep)F\u{001B}[0J", terminator: "")
+				print("\u{001B}[\(numLinesToShow)F\u{001B}[0J", terminator: "")
 				printFrom = 0
 			}
 			for line in outputLines[printFrom...] {
@@ -110,5 +155,81 @@ func runCommand(_ command: String, arguments: [String], prefix: String) async th
 	return process.terminationStatus
 }
 
+func parseArguments(_ args: [String]) -> CliOptions {
+	var options: CliOptions = CliOptions(commandToExec: [])
 
-await main()
+	var parsingOptions = true
+
+	// First one is the script name, so we skip it
+	for arg in args.dropFirst() {
+		if !parsingOptions {
+			options.commandToExec.append(arg)
+			continue
+		}
+
+		if arg == "--" {
+			parsingOptions = false
+			continue
+		}
+
+		var key: String;
+		var value: String;
+
+		if arg.hasPrefix("-") && !arg.hasPrefix("--") {
+			// Short option
+			let option = String(arg.dropFirst())
+			if option.count == 1 {
+				key = option
+				value = ""
+			} else {
+				key = String(option.prefix(1))
+				value = String(option.dropFirst())
+			}
+		} else if arg.hasPrefix("--") && arg.contains("=") {
+			// Long option with value
+			let eqIdx = arg.firstIndex(of: "=")!
+			key = String(arg.dropFirst(2).prefix(upTo: eqIdx))
+			value = String(arg[arg.index(after: eqIdx)...])
+		} else if arg.hasPrefix("--") && !arg.contains("=") {
+			// Long option without value
+			key = String(arg.dropFirst(2))
+			value = ""
+		} else {
+			// Not an option. This must be first first arg that starts the command
+			parsingOptions = false
+			options.commandToExec.append(arg)
+			continue
+		}
+
+		assert(!key.isEmpty, "Option key cannot be empty")
+
+		if ["h", "help"].contains(key) {
+			print("""
+			Usage: watch-urls-in-pasteboard.swift [options] -- [command ...]
+
+			Options:
+			  -h, --help               Show this help message and exit
+			  --lines-to-show=N, -l N  Number of lines to show in the output for each command.
+
+			After the -- separator, you can specify an external command and its arguments.
+			""")
+			exit(0)
+		} else if ["l", "--lines-to-show"].contains(key) {
+			// Handle lines to show option
+			if let numLinesToShow = UInt(value) {
+				options.numLinesToShow = numLinesToShow
+			} else {
+				print("Invalid value for --lines-to-show: \(value)")
+				exit(1)
+			}
+		} else {
+			print("Unknown option: \(key)")
+			exit(1)
+		}
+	}
+
+	return options
+}
+
+
+await main(options: parseArguments(CommandLine.arguments))
