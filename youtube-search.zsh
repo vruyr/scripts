61511f2ddf4d601e94e31299c5625893ce9ef706
Vruyr Gyolchanyan <vruyr@vruyr.com>
2026-03-14 13:23:34 -0400

    Added thumbnail support to `youtube-search.zsh`.

--- youtube-search.zsh
+++ youtube-search.zsh
@@ -3,9 +3,9 @@
 youtube-search () {
     setopt local_options err_return
     local output_format=$'\e[100;97m\e]8;;%(webpage_url)s\a%(id)-11s\e]8;;\a\e[0m \e[42m%(upload_date>%Y-%m-%d)-10s\e[0m \e[103;30m%(view_count&{:>11,})s\e[0m \e[103;30m%(duration_string&{:>8})s\e[0m \e[46m%(uploader)-20s\e[0m %(title)s'
-    local channel=() use_fzf= num_results_opt=()
+    local channel=() use_fzf= use_thumbnails= num_results_opt=()
 
-    zparseopts -D -E -- c:=channel -channel:=channel -fzf=use_fzf n:=num_results_opt -num-results:=num_results_opt
+    zparseopts -D -E -- c:=channel -channel:=channel -fzf=use_fzf t=use_thumbnails -thumbnails=use_thumbnails n:=num_results_opt -num-results:=num_results_opt
 
     local num_results=${num_results_opt[-1]:-0}
 
@@ -31,6 +31,63 @@ youtube-search () {
     [[ -n "$channel" && "$num_results" -gt 0 ]] && extra_opts=(--max-downloads "$num_results")
 
     local yt_cmd=(uv tool run -q yt-dlp@latest --no-warnings --skip-download --quiet "${extra_opts[@]}" --print "$output_format" "$source")
+
+    if [[ -n "$use_thumbnails" ]]; then
+        local thumb_cols=40 thumb_rows=11
+        local cell_height_px=20 cell_width_px=10  # defaults, overridden by terminal query below
+        {
+            local stty_save=$(stty -g </dev/tty)
+            stty -echo -icanon </dev/tty
+            printf '\e[16t' >/dev/tty
+            local cell_resp=
+            IFS= read -d 't' -rs -t 1 cell_resp </dev/tty
+            stty $stty_save </dev/tty
+            local cell_parts=(${(s:;:)cell_resp})
+            cell_height_px=${cell_parts[2]:-20}
+            cell_width_px=${cell_parts[3]:-10}
+        } 2>/dev/null || true
+
+        local thumb_format=$'%(thumbnail)s\t%(webpage_url)s\t%(id)s\t%(upload_date>%Y-%m-%d)s\t%(view_count&{:,})s\t%(duration_string)s\t%(uploader)s\t%(title)s'
+
+        uv tool run -q yt-dlp@latest --no-warnings --skip-download --quiet \
+            "${extra_opts[@]}" --print "$thumb_format" "$source" \
+        | while IFS=$'\t' read -r thumbnail url id date views duration uploader title; do
+            local tmp=$(mktemp /tmp/yt-thumb-XXXXXX)
+            curl -fsSL "$thumbnail" -o "$tmp" 2>/dev/null || { rm -f "$tmp"; continue; }
+            local img_dims=($(sips -g pixelWidth -g pixelHeight "$tmp" 2>/dev/null | awk '/pixel/{print $2}'))
+            local img_w=${img_dims[1]} img_h=${img_dims[2]}
+            local scaled_cols=$(( (img_w * thumb_rows * cell_height_px + img_h * cell_width_px - 1) / (img_h * cell_width_px) ))
+            local info_col=$(( (scaled_cols < thumb_cols ? scaled_cols : thumb_cols) + 1 ))
+            timg -g "${thumb_cols}x${thumb_rows}" "$tmp"
+            rm -f "$tmp"
+
+            local today_epoch=$(date +%s)
+            local video_epoch=$(date -j -f "%Y-%m-%d" "$date" +%s 2>/dev/null || date -d "$date" +%s)
+            local days_ago=$(( (today_epoch - video_epoch) / 86400 ))
+            local rel=
+            if   (( days_ago <  7  )); then rel="${days_ago} days ago"
+            elif (( days_ago <  30 )); then rel="$((days_ago / 7)) weeks ago"
+            elif (( days_ago < 365 )); then rel="$((days_ago / 30)) months ago"
+            else                            rel="$((days_ago / 365)) years ago"
+            fi
+
+            local -a info_lines=(
+                $'\e]8;;'"${url}"$'\a\e[1m'"${title}"$'\e[0m\e]8;;\a'
+                "${duration}"
+                "${date}  (${rel})"
+                "${views} views"
+                "${uploader}"
+                "${id}"
+            )
+            printf "\e[${thumb_rows}A"
+            for line in "${info_lines[@]}"; do
+                printf "\e[${info_col}C%s\n" "$line"
+            done
+            (( thumb_rows > ${#info_lines[@]} )) && printf "\e[$((thumb_rows - ${#info_lines[@]}))B"
+            printf "\n"
+        done
+        return
+    fi
 
     local col_header=$(uv run --python 3 python -c "
 import sys
